1a97c01252b9f688258ab08d6bf71496499285bc
stacysorey <stacysorey@gmail.com>
2020-11-12 16:37:30 -0500

    commit tree 54ce0536c2d0ae480d6aa51ef5c0ae2a7ee26498

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    badges = []
+    counter = 0
+    attendees.each do |guests| 
+        badges << "Hello, my name is #{guests}." 
+        counter += 1
+    end
+    return badges
+end
+
+def assign_rooms(attendees)
+    room_assignments = []
+    counter = 0
+    attendees.each_with_index do |guests, index|
+        room_assignments << "Hello, #{guests}! You'll be assigned to room #{index + 1}!"
+        counter += 1
+    end
+    return room_assignments
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each {|printout| puts printout}
+    assign_rooms(attendees).each {|printout| puts printout}
+end
